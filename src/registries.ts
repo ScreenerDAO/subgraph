be09b98b80de7b34f464cedf1e5099d81e6f2141
AlwaysBuyCheap <pablodh6@gmail.com>
2022-12-22 17:01:22 +0100

    ...

--- src/registries.ts
+++ src/registries.ts
@@ -1,105 +1,65 @@
 import {
-  CompanyAdded as CompanyAddedEvent,
-  CompanyDataEdited as CompanyDataEditedEvent,
-  CompanyEdited as CompanyEditedEvent,
-  CompanyNameEdited as CompanyNameEditedEvent,
-  CompanyTickerEdited as CompanyTickerEditedEvent,
-  Initialized as InitializedEvent
+    CompanyAdded as CompanyAddedEvent,
+    CompanyDataEdited as CompanyDataEditedEvent,
+    CompanyEdited as CompanyEditedEvent,
+    CompanyNameEdited as CompanyNameEditedEvent,
+    CompanyTickerEdited as CompanyTickerEditedEvent,
+    Initialized as InitializedEvent
 } from "../generated/Registries/Registries"
-import {
-  CompanyAdded,
-  CompanyDataEdited,
-  CompanyEdited,
-  CompanyNameEdited,
-  CompanyTickerEdited,
-  Initialized
-} from "../generated/schema"
+import { Company } from "../generated/schema"
 
 export function handleCompanyAdded(event: CompanyAddedEvent): void {
-  let entity = new CompanyAdded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.id = event.params.id
-  entity.name = event.params.name
-  entity.ticker = event.params.ticker
-  entity.dataHash = event.params.dataHash
+    //   let entity = new CompanyAdded(
+    //     event.transaction.hash.concatI32(event.logIndex.toI32())
+    //   )
+    //   entity.id = event.params.id
+    //   entity.name = event.params.name
+    //   entity.ticker = event.params.ticker
+    //   entity.dataHash = event.params.dataHash
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+    //   entity.blockNumber = event.block.number
+    //   entity.blockTimestamp = event.block.timestamp
+    //   entity.transactionHash = event.transaction.hash
 
-  entity.save()
+    let entity = new Company(event.params.id.toString())
+
+    entity.name = event.params.name
+    entity.ticker = event.params.ticker
+    entity.dataHash = event.params.dataHash
+
+    entity.save()
 }
 
 export function handleCompanyDataEdited(event: CompanyDataEditedEvent): void {
-  let entity = new CompanyDataEdited(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.id = event.params.id
-  entity.dataHash = event.params.dataHash
+    let entity = Company.load(event.params.id.toString())
+    entity!.dataHash = event.params.dataHash
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+    entity!.save()
 }
 
 export function handleCompanyEdited(event: CompanyEditedEvent): void {
-  let entity = new CompanyEdited(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.id = event.params.id
-  entity.name = event.params.name
-  entity.ticker = event.params.ticker
-  entity.dataHash = event.params.dataHash
+    let entity = Company.load(event.params.id.toString())
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+    entity!.name = event.params.name
+    entity!.ticker = event.params.ticker
+    entity!.dataHash = event.params.dataHash
 
-  entity.save()
+    entity!.save()
 }
 
 export function handleCompanyNameEdited(event: CompanyNameEditedEvent): void {
-  let entity = new CompanyNameEdited(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.id = event.params.id
-  entity.name = event.params.name
+    let entity = Company.load(event.params.id.toString())
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+    entity!.name = event.params.name
 
-  entity.save()
+    entity!.save()
 }
 
-export function handleCompanyTickerEdited(
-  event: CompanyTickerEditedEvent
-): void {
-  let entity = new CompanyTickerEdited(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.id = event.params.id
-  entity.ticker = event.params.ticker
+export function handleCompanyTickerEdited(event: CompanyTickerEditedEvent): void {
+    let entity = Company.load(event.params.id.toString())
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+    entity!.ticker = event.params.ticker
 
-  entity.save()
+    entity!.save()
 }
 
-export function handleInitialized(event: InitializedEvent): void {
-  let entity = new Initialized(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.version = event.params.version
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
